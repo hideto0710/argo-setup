3bde359535b9566d074d070020f3df5b051ad05b
hideto_inamura <hideto_inamura@r.recruit.co.jp>
2018-12-27 11:34:38 +0900

    remove workflow-crd

--- setup.sh
+++ setup.sh
@@ -17,3 +17,15 @@ brew install --build-from-source ./argocd.rb
 kubectl port-forward svc/argocd-server 8081:80 -n argocd
 kubectl get pods -n argocd -l app=argocd-server -o name | cut -d'/' -f 2
 argocd login localhost:8081
+
+# argo-events
+argocd repo add https://github.com/hideto0710/argo-setup.git
+kubectl create ns argo
+argocd app create argo-events \
+    --repo https://github.com/hideto0710/argo-setup.git \
+    --path events \
+    --dest-namespace argo \
+    --dest-server https://kubernetes.default.svc
+
+# others
+kubectl port-forward svc/argo-ui 8080:80 -n argo
